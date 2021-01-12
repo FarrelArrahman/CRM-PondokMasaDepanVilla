03e73563478624a84111bbc23be01445b181fefd
FarrelArrahman <arrahman_farrel@yahoo.com>
2021-01-12 09:01:15 +0800

    Added attributes in UlasanMasukan model

--- app/Models/UlasanMasukan.php
+++ app/Models/UlasanMasukan.php
@@ -8,4 +8,49 @@ use Illuminate\Database\Eloquent\Model;
 class UlasanMasukan extends Model
 {
     use HasFactory;
+
+    protected $table = 'tb_ulasan_masukan';
+    protected $primaryKey = 'id_ulasan_masukan';
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_responden',
+        'id_periode',
+        'ulasan_masukan',
+        'tgl_input',
+        'keterangan',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        // 
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        //
+    ];
+
+    public function responden()
+    {
+        return $this->hasOne(Responden::class, 'id_responden', 'id_responden');
+    }
+
+    public function periode()
+    {
+        return $this->hasOne(Periode::class, 'id_periode', 'id_periode');
+    }
 }
