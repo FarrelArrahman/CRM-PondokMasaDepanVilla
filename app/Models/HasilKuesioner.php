b805f5783f114245a6c1ce74fc2e58ee243cc124
FarrelArrahman <arrahman_farrel@yahoo.com>
2021-01-12 09:00:49 +0800

    Added attributes in HasilKuesioner model

--- app/Models/HasilKuesioner.php
+++ app/Models/HasilKuesioner.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class HasilKuesioner extends Model
 {
     use HasFactory;
+
+    protected $table = 'tb_hasil_kuesioner';
+    protected $primaryKey = 'id_hasil_kuesioner';
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_responden',
+        'id_pertanyaan',
+        'nilai',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        // 
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        //
+    ];
 }
