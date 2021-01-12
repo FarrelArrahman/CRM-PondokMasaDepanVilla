e88938c81ca2be0212e24eb644988ffeabffd5b6
FarrelArrahman <arrahman_farrel@yahoo.com>
2021-01-12 09:00:59 +0800

    Added attributes in Responden model

--- app/Models/Responden.php
+++ app/Models/Responden.php
@@ -8,4 +8,41 @@ use Illuminate\Database\Eloquent\Model;
 class Responden extends Model
 {
     use HasFactory;
+
+    protected $table = 'tb_responden';
+    protected $primaryKey = 'id_responden';
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama_responden',
+        'email',
+        'no_hp',
+        'jenis_kel',
+        'alamat',
+        'status',
+        'keterangan',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        // 
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        //
+    ];
 }
